7d7d7e3ed8dfd63a618769af70af62b9ea486287
Eternatus9999 <eternatus9999@gmail.com>
2024-11-01 16:07:43 +0530

    latest commit of frontend

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AddCustomerPageComponent } from "./page/add-customer-page/add-customer-page.component";
+import { ManageCustomerPageComponent } from "./page/manage-customer-page/manage-customer-page.component";
+import { NavBarComponent } from "./common/nav-bar/nav-bar.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, AddCustomerPageComponent, ManageCustomerPageComponent, NavBarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'customer-manage-front-end';
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { AddCustomerPageComponent } from './page/add-customer-page/add-customer-page.component';
+import { ManageCustomerPageComponent } from './page/manage-customer-page/manage-customer-page.component';
+
+export const routes: Routes = [
+    {
+        path:"add-customer",
+        component:AddCustomerPageComponent
+    },
+    {
+        path:"manage-customer",
+        component:ManageCustomerPageComponent
+    }
+];
